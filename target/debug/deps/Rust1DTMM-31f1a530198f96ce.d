b5a99a969f5403df4dda74af68dff1d71bebdcc3
Naren Ganesh <nareng2026@gmail.com>
2025-04-09 17:47:22 -0500

    first run

---
+++ target/debug/deps/Rust1DTMM-31f1a530198f96ce.d
@@ -0,0 +1,5 @@
+/Users/narenganesh/Rust1DTMM/target/debug/deps/Rust1DTMM-31f1a530198f96ce: src/main.rs
+
+/Users/narenganesh/Rust1DTMM/target/debug/deps/Rust1DTMM-31f1a530198f96ce.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/num_integer-a59591f93d483f89.d
@@ -0,0 +1,9 @@
+/Users/narenganesh/Rust1DTMM/target/debug/deps/libnum_integer-a59591f93d483f89.rmeta: /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
+
+/Users/narenganesh/Rust1DTMM/target/debug/deps/libnum_integer-a59591f93d483f89.rlib: /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
+
+/Users/narenganesh/Rust1DTMM/target/debug/deps/num_integer-a59591f93d483f89.d: /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
+
+/Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs:
+/Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs:
+/Users/narenganesh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs:
