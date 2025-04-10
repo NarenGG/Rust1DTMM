6d46c72be307a40bc30f7a2b746508adf689d86c
Naren Ganesh <nareng2026@gmail.com>
2025-04-10 11:54:20 -0500

    fixed initSync call

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import init, { initSync } from './pkg/Rust1DTMM.js';
 
 async function run() {
     await init(); // Initialize the Wasm module
-    console.log(initSync(5)); // Call the Rust function from JavaScript
+    console.log(initSync()); // Call the Rust function from JavaScript
 }
 
 run();
