17d0a73626f0364dd22cf2a01e3002bb23646186
Naren Ganesh <nareng2026@gmail.com>
2025-04-10 11:29:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import init, { solve_tmm_js } from './pkg/Rust1DTMM.js';
 
 async function run() {
     await init(); // Initialize the Wasm module
-    console.log(calculate(5)); // Call the Rust function from JavaScript
+    console.log(solve_tmm_js(5)); // Call the Rust function from JavaScript
 }
 
 run();
