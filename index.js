d448084fc019a34ea3bf2538dfd679c6ed6e0aaa
Naren Ganesh <nareng2026@gmail.com>
2025-04-10 13:19:03 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import init, { calculate } from './pkg/Rust1DTMM.js';
-
-async function run() {
-    await init(); // Initialize the Wasm module
-    console.log(calculate(5)); // Call the Rust function from JavaScript
-}
-
-run();
